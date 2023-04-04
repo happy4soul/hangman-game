6e6c78163853a37909b0f11eb1e920374e222090
Bhaskar <98847406+happy4soul@users.noreply.github.com>
2023-04-05 02:06:41 +0530

    code updated

--- hangman.js
+++ hangman.js
@@ -95,19 +95,4 @@ Hangman.prototype.gameStatus = function(){
 
 
 
-let word1 = new Hangman('Sarada Uchiha', 5)
 
-
-
-console.log(word1.getPuzzleBack())
-console.log(word1.guess)
-
-
-
-window.addEventListener('keydown', function (e) {
-    let guessedWord = e.key
-    word1.makeGuess(guessedWord)
-    console.log(word1.getPuzzleBack())
-    console.log(word1.gameDetails())
-    console.log(word1.gameStatus())
-})
